eebaea9623fcd5bdeca88c440ea49617270019a5
Roland Bewick <roland.bewick@gmail.com>
2024-10-08 13:54:37 +0700

    fix: properly follow LNURL spec and add LNURL error handling

--- src/lnurlp.ts
+++ src/lnurlp.ts
@@ -8,50 +8,61 @@ import "./nwc/nwcPool.ts";
 export function createLnurlApp(db: DB) {
   const hono = new Hono();
 
-  hono.get("/:username", (c) => {
-    const username = c.req.param("username");
+  hono.get("/:username", async (c) => {
+    try {
+      const username = c.req.param("username");
 
-    logger.debug("LNURLp request", { username });
+      logger.debug("LNURLp request", { username });
 
-    // TODO: zapper support
+      // check the user exists
+      await db.findWalletConnectionSecret(username);
 
-    return c.json({
-      status: "OK",
-      tag: "payRequest",
-      commentAllowed: 255,
-      callback: `${BASE_URL}/.well-known/lnurlp/${username}/callback`,
-      minSendable: 1000,
-      maxSendable: 10000000000,
-      metadata: `[["text/identifier","${username}@${DOMAIN}"],["text/plain","Sats for ${username}"]]`,
-    });
+      // TODO: zapper support
+
+      return c.json({
+        tag: "payRequest",
+        commentAllowed: 255,
+        callback: `${BASE_URL}/.well-known/lnurlp/${username}/callback`,
+        minSendable: 1000,
+        maxSendable: 10000000000,
+        metadata: `[["text/identifier","${username}@${DOMAIN}"],["text/plain","Sats for ${username}"]]`,
+      });
+    } catch (error) {
+      return c.json({ status: "ERROR", reason: "" + error });
+    }
   });
 
   hono.get("/:username/callback", async (c) => {
-    const username = c.req.param("username");
-    const amount = c.req.query("amount");
-    const comment = c.req.query("comment") || "";
-    logger.debug("LNURLp callback", { username, amount, comment });
+    try {
+      const username = c.req.param("username");
+      const amount = c.req.query("amount");
+      const comment = c.req.query("comment") || "";
+      logger.debug("LNURLp callback", { username, amount, comment });
 
-    // TODO: store data (e.g. for zaps)
+      // TODO: store data (e.g. for zaps)
 
-    if (!amount) {
-      return c.text("No amount provided", 404);
+      if (!amount) {
+        throw new Error("No amount provided");
+      }
+
+      const connectionSecret = await db.findWalletConnectionSecret(username);
+
+      const nwcClient = new nwc.NWCClient({
+        nostrWalletConnectUrl: connectionSecret,
+      });
+
+      const transaction = await nwcClient.makeInvoice({
+        amount: +amount,
+        description: comment,
+      });
+
+      return c.json({
+        pr: transaction.invoice,
+        routes: [],
+      });
+    } catch (error) {
+      return c.json({ status: "ERROR", reason: "" + error });
     }
-
-    const connectionSecret = await db.findWalletConnectionSecret(username);
-
-    const nwcClient = new nwc.NWCClient({
-      nostrWalletConnectUrl: connectionSecret,
-    });
-
-    const transaction = await nwcClient.makeInvoice({
-      amount: +amount,
-      description: comment,
-    });
-
-    return c.json({
-      pr: transaction.invoice,
-    });
   });
   return hono;
 }
