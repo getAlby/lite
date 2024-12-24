d24748c1790cf16cfb99fbc8df27f9eb6aa90713
im-adithya <imadithyavardhan@gmail.com>
2024-12-24 11:42:40 +0530

    chore: make nostr pubkey column not null

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,7 +4,7 @@ export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   encryptedConnectionSecret: text("connection_secret").notNull(),
   username: text("username").unique().notNull(),
-  nostrPubkey: text("nostr_pubkey"),
+  nostrPubkey: text("nostr_pubkey").notNull(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
 });
 
